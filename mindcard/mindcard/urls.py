108327ec78074acf392caea10449b79bfa4a3ef8
dayze3 <daisy_hu@live.com>
2021-08-14 15:15:33 -0700

    changed urls.py to prevent initial 404

--- mindcard/mindcard/urls.py
+++ mindcard/mindcard/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mindcardapp/', include("mindcardapp.urls"))
+    path('/', include("mindcardapp.urls")),
 ]
